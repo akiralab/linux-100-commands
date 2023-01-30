8f111e75c6346b687ef08b720cd2454d73bdec8c
Akira Kawai <kawaiakira51@gmail.com>
2023-01-31 00:42:34 +0900

    jbook修正

--- _build/jupyter_execute/ipynb/14_sleep.py
+++ _build/jupyter_execute/ipynb/14_sleep.py
@@ -1,49 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## xxxxxx コマンド
-# - {名称の由来}から名称がきている
-# - {コマンド内容}
+# ## sleep コマンド
+# - 一定時間休止する
 
 # ---
-# `commands -options` *FILENAME*： *FILENAME*にxxxする
+# `sleep` *TIME(sec)*： *TIME(sec)*休止する
 
 # In[1]:
 
 
-get_ipython().system('xx')
-
-
-# ---
-# `commands -options` *FILENAME*： *FILENAME*にxxxする
-
-# In[2]:
-
-
-get_ipython().system('xx -a')
-
-
-# ---
-# `commands -options` *FILENAME*： *FILENAME*にxxxする
-
-# In[3]:
-
-
-get_ipython().system('xx -l')
-
-# 
-
-L
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+get_ipython().system('sleep 10')
 
 
 # In[ ]:
